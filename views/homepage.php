07839a79736243d2c59a695954e724b3f5953370
Marc <cuendem05@gmail.com>
2024-11-06 18:42:53 +0100

    Progress saving before applying Bootstrap seriously

--- views/homepage.php
+++ views/homepage.php
@@ -1,8 +1,8 @@
 <?php include_once('header.php') ?>
 <main>
-    <section class="banner">
+    <section id="banner">
         <div class="bannerText">
-            <h1>Delicious. Customizable. Yours.</h1>
+            <h1>Build whatever.<br>Eat wherever.</h1>
             <p>Build your very own container. Unlimited combinations.</p>
         </div>
         <div class="bannerButtons">
@@ -14,21 +14,53 @@
         </div>
     </section>
 
-    <section class="whatis">
-        <div class="whatisText">
-            <h2>What is DockerEats?</h2>
-            <h3>A completely customizable experience for every customer</h3>
-            <p>DockerEats is the latest branch in the Docker family, a fun and engaging way to adapt our love for containerized applications into the second most important helper for a developer — Food!</p>
-        </div>
-        <div class="whatisButtons">
-
-        </div>
-        <div class="whatisBody">
-            <div class="whatisBodyText">
-
+    <section id="whatis">
+        <div class="whatisContainer">
+            <div class="whatisText">
+                <h2>What is DockerEats?</h2>
+                <h3>A completely customizable experience for every customer</h3>
+                <p>DockerEats is the latest branch in the Docker family, a fun and engaging way to adapt our love for containerized applications into the second most important helper for a developer — Food!</p>
             </div>
-            <div class="whatisBodyImage">
-
+            <div class="whatisButtons">
+                <button type="button" class="btn btn-secondary btn-selected">Containers</button>
+                <button type="button" class="btn btn-secondary btn-normal">Customize</button>
+                <button type="button" class="btn btn-secondary btn-normal">Purchase</button>
+                <button type="button" class="btn btn-secondary btn-normal">Deliver</button>
+            </div>
+            <div class="whatisBody">
+                <div class="whatisBodyText">
+                    <h4>Containers</h4>
+                    <div class="textPar">
+                        <h5>A container with a whole course meal inside</h5>
+                        <p>A DockerEats container has all the necessary ingredients, parts and preparation for an entire meal, all in one small package. Plus, you'll be able to build your own container, customizing your culinary experience to the max.</p>
+                    </div>
+                    <div class="textPar">
+                        <h5>Healthy, compact and light</h5>
+                        <p>Our DockerEats brand containers are extremely light and easily transported, keep food fresh, hot (or cold!) whatever the weather may be. You can even keep them after the food has been eaten to use them as normal containers!</p>
+                    </div>
+                    <div class="textPar">
+                        <h5>Freshly-made, freshly-eaten</h5>
+                        <p>Our delivery is quick to make sure your food is as fresh when it gets to you as it was when it left our kitchen. Precooked is not in our dictionary — everything you eat has been freshly prepared and cooked the same day!</p>
+                    </div>
+                </div>
+                <br>
+                <div class="whatisBodyImage">
+                    <img src="/img/TupperContainer.png" class="undraggable" alt="">
+                </div>
+            </div>
+            <div class="whatisButtons2">
+                <button type="button" class="btn btn-selected">View the menu</button>
+                <button type="button" class="btn btn-normal">Our ingredients</button>
+            </div>
+        </div>
+    </section>
+    <section id="whyorder">
+        <div class="whyorderContainer">
+            <div class="whyorderText">
+                <h2>Why order DockerEats?</h2>
+                <h3>Already trusted by developers.<br>Now trusted by consumers.</h3>
+                <p>DockerEats allows you to eat whatever you want, whenever and wherever you want. And most importantly, eat it the way you want. Much like our containers, we completely adapt to our customer's wants and needs.</p>
+                <a href="#">Read our reviews ➜</a>
             </div>
         </div>
     </section>
