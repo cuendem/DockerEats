fb262b29ee18f12af4e980bc3396bf29cd5243b3
Marc <cuendem05@gmail.com>
2024-11-08 20:15:52 +0100

    Initial Reviews

--- views/header.php
+++ views/header.php
@@ -8,8 +8,8 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mx-auto mb-2 mb-lg-0 align-items-center">
-                    <li class="nav-item dropdown px-3">
+                <ul class="navbar-nav mx-auto mb-2 mb-lg-0 align-items-center gap-4">
+                    <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Containers
                             <svg class="dropdown-chevron" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6 9L12 15L18 9" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
@@ -23,17 +23,17 @@
                             <li><a class="dropdown-item" href="#">Desserts</a></li>
                         </ul>
                     </li>
-                    <li class="nav-item px-3">
+                    <li class="nav-item">
                         <a class="nav-link" href="#">Our Ingredients</a>
                     </li>
-                    <li class="nav-item px-3">
+                    <li class="nav-item">
                         <a class="nav-link" href="#">Special Offers</a>
                     </li>
-                    <li class="nav-item px-3">
+                    <li class="nav-item">
                         <a class="nav-link" href="#">About Us</a>
                     </li>
                 </ul>
-                <div class="navbar-right d-flex align-items-center">
+                <div class="navbar-right nav-item d-flex align-items-center">
                     <svg class="me-2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M14.9536 14.9458L21 21M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></g></svg>
                     <button type="button" class="btn btn-normal">Sign In</button>
                 </div>
--- views/homepage.php
+++ views/homepage.php
@@ -16,13 +16,13 @@
         </div>
     </section>
 
-    <section id="whatis" class="container-fluid">
+    <section id="whatis" class="container-fluid wave-separator">
         <div class="row">
             <div class="col-md-6 col-lg-8 text-center mx-auto">
                 <h2>What is DockerEats?</h2>
                 <h3>A completely customizable experience for every customer</h3>
                 <p>DockerEats is the latest branch in the Docker family, a fun and engaging way to adapt our love for containerized applications into the second most important helper for a developer — Food!</p>
-                <div class="d-flex justify-content-center mt-5 gap-3">
+                <div class="d-flex justify-content-center mt-4 gap-3">
                     <button type="button" class="btn btn-secondary btn-selected">Containers</button>
                     <button type="button" class="btn btn-secondary btn-normal">Customize</button>
                     <button type="button" class="btn btn-secondary btn-normal">Purchase</button>
@@ -30,40 +30,110 @@
                 </div>
             </div>
         </div>
-        <div class="whatisBody row mt-5">
-            <div class="col-lg-6">
+        <div class="whatisBody row">
+            <div class="col-xl-6">
                 <h4>Containers</h4>
-                <div class="textPar">
+                <div class="mt-4">
                     <h5>A container with a whole course meal inside</h5>
                     <p>A DockerEats container has all the necessary ingredients, parts and preparation for an entire meal, all in one small package. Plus, you'll be able to build your own container, customizing your culinary experience to the max.</p>
                 </div>
-                <div class="textPar">
+                <div class="my-5">
                     <h5>Healthy, compact and light</h5>
                     <p>Our DockerEats brand containers are extremely light and easily transported, keep food fresh, hot (or cold!) whatever the weather may be. You can even keep them after the food has been eaten to use them as normal containers!</p>
                 </div>
-                <div class="textPar">
+                <div>
                     <h5>Freshly-made, freshly-eaten</h5>
-                    <p>Our delivery is quick to make sure your food is as fresh when it gets to you as it was when it left our kitchen. Precooked is not in our dictionary — everything you eat has been freshly prepared and cooked the same day!</p>
+                    <p class="mb-0">Our delivery is quick to make sure your food is as fresh when it gets to you as it was when it left our kitchen. Precooked is not in our dictionary — everything you eat has been freshly prepared and cooked the same day!</p>
                 </div>
             </div>
-            <br>
-            <div class="whatisBodyImage col-lg-6">
-                <img src="/img/TupperContainer.png" class="undraggable" alt="">
+            <div class="whatisImage col-xl-6">
+                <img src="/img/TupperContainer.png" class="undraggable" alt="DockerEats Container">
             </div>
         </div>
-        <div class="row">
-            <button type="button" class="btn btn-selected">View the menu</button>
-            <button type="button" class="btn btn-normal">Our ingredients</button>
+        <div class="row d-flex">
+            <div class="col d-flex mt-5 justify-content-center">
+                <div class="d-flex gap-3">
+                    <button type="button" class="btn btn-selected">View the menu</button>
+                    <button type="button" class="btn btn-normal">Our ingredients</button>
+                </div>
+            </div>
         </div>
     </section>
-    <section id="whyorder">
-        <div class="whyorderContainer">
-            <div class="whyorderText">
+
+    <section id="whyorder" class="container-fluid wave-separator">
+        <div class="row">
+            <div class="col-lg-8 text-center mx-auto">
                 <h2>Why order DockerEats?</h2>
                 <h3>Already trusted by developers.<br>Now trusted by consumers.</h3>
                 <p>DockerEats allows you to eat whatever you want, whenever and wherever you want. And most importantly, eat it the way you want. Much like our containers, we completely adapt to our customer's wants and needs.</p>
                 <a href="#">Read our reviews ➜</a>
             </div>
+        </div>
+        <div class="my-5 d-flex">
+            <div class="card review mx-auto pl-0 col-lg-4">
+                <div class="row g-0">
+                    <div class="col-3 p-3 rounded">
+                        <img src="/img/products/product1.png" class="img-fluid" alt="Jeremy Elbertson">
+                    </div>
+                    <div class="col-9 d-flex align-items-center">
+                        <div class="card-body">
+                            <h5 class="card-title">Jeremy Elbertson</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary"><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-half"></i><i class="bi bi-star"></i></h6>
+                            <p class="card-text">Ever since I tried DockerEats, I never eat anywhere else. Not even my home.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card review mx-auto pl-0 col-lg-4">
+                <div class="row g-0">
+                    <div class="col-3 p-3 rounded">
+                        <img src="/img/products/product2.png" class="img-fluid" alt="Jeremiah Albert Jr.">
+                    </div>
+                    <div class="col-9 d-flex align-items-center">
+                        <div class="card-body">
+                            <h5 class="card-title">Jeremiah Albert Jr.</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary"><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-half"></i></h6>
+                            <p class="card-text">Just perfect. The degree of customization is amazing. You are in control of everything.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card review mx-auto pl-0 col-lg-4">
+                <div class="row g-0">
+                    <div class="col-3 p-3 rounded">
+                        <img src="/img/products/product3.png" class="img-fluid" alt="José Alberto Olivares">
+                    </div>
+                    <div class="col-9 d-flex align-items-center">
+                        <div class="card-body">
+                            <h5 class="card-title">José Alberto Olivares</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary"><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star"></i></h6>
+                            <p class="card-text">Containers are useful even after having finished your food. They can store things.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="siteNumbers row text-center mt-5">
+            <div class="col-md-4">
+                <h3>5K+</h3>
+                <p>customers</p>
+            </div>
+            <div class="col-md-4">
+                <h3>10K+</h3>
+                <p>monthly orders</p>
+            </div>
+            <div class="col-md-4">
+                <h3>1M+</h3>
+                <p>containers built so far</p>
+            </div>
+        </div>
+    </section>
+
+    <section id="getstarted" class="container-fluid wave-separator">
+        <div class="row">
+            <h2>How to get started</h2>
+            <h3>Hungry? Order now and eat in a completely customizable way.</h3>
         </div>
     </section>
 </main>
